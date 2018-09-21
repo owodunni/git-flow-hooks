98a492e2213147160f177c1066b54bb729f2054e
Alexander Poole <alex.o.poole@gmail.com>
2018-09-21 18:34:47 +0200

    Prevent push to development branch

--- modules/prevent-master-commits.sh
+++ modules/prevent-master-commits.sh
@@ -2,8 +2,8 @@
 
 CURRENT_BRANCH=$(git symbolic-ref HEAD)
 
-if [ "$CURRENT_BRANCH" == "refs/heads/master" ]; then
-    __print_fail "Direct commits to the master branch are not allowed."
+if [ "$CURRENT_BRANCH" == "refs/heads/master" ] || [ "$CURRENT_BRANCH" == "refs/heads/develop]; then
+    __print_fail "Direct commits to the master or dev branch are not allowed."
     return 1
 else
     return 0
