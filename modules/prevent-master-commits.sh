0f47cf3974b07bcdf88fd395aadcac93b9401fa1
Alexander Poole <alex.o.poole@gmail.com>
2018-09-21 18:47:59 +0200

    Fixed an error

--- modules/prevent-master-commits.sh
+++ modules/prevent-master-commits.sh
@@ -2,7 +2,7 @@
 
 CURRENT_BRANCH=$(git symbolic-ref HEAD)
 
-if [ "$CURRENT_BRANCH" == "refs/heads/master" ] || [ "$CURRENT_BRANCH" == "refs/heads/develop]; then
+if [ "$CURRENT_BRANCH" == "refs/heads/master" ] || [ "$CURRENT_BRANCH" == "refs/heads/develop" ]; then
     __print_fail "Direct commits to the master or dev branch are not allowed."
     return 1
 else
